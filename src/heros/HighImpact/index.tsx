a17cc7189bf96befec4d8d9298a1d531750a9a3a
flashworth <paul@paulmc.xyz>
2025-04-17 09:05:32 +0100

    committing small changes

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -15,9 +15,11 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     setHeaderTheme('dark')
   })
 
+  // -mt-[10.4rem]
+
   return (
     <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
+      className="relative -mt-[10.4rem] flex items-center justify-center text-white bg-[oklch(0.21 0.0362 207.44)]"
       data-theme="dark"
     >
       <div className="container mb-8 z-10 relative flex items-center justify-center">
